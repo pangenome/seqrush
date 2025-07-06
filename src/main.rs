b2b04497d529f08d037dd54c790c6d945403f66a
Erik Garrison <erik.garrison@gmail.com>
2025-07-06 19:28:09 +0200

    merging, thanks Kristopher!

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,7 @@
-use seqrush::{run_seqrush, Args};
-
-#[cfg(feature = "cli")]
-mod cli;
+use clap::Parser;
+use seqrush::seqrush::{Args, run_seqrush};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    #[cfg(feature = "cli")]
-    let args = cli::parse();
-
-    #[cfg(not(feature = "cli"))]
-    let args = {
-        let mut iter = std::env::args().skip(1);
-        let sequences = iter.next().expect("input FASTA required");
-        let output = iter.next().expect("output file required");
-        Args {
-            sequences,
-            output,
-            threads: 1,
-            min_match_length: 15,
-        }
-    };
-
-    run_seqrush(args)?;
-    Ok(())
+    let args = Args::parse();
+    run_seqrush(args)
 }
